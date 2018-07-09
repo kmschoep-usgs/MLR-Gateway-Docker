7321fdcf4d71458c27c28068973d2ef338793593
Kathryn Schoephoester <kmschoep@usgs.gov>
2018-07-09 11:54:10 -0500

    MLR-274: add DB password config to launch-app.sh

--- launch-app.sh
+++ launch-app.sh
@@ -5,6 +5,12 @@ if [ -n "${OAUTH_CLIENT_SECRET_PATH}" ]; then
   oauthClientSecret=$(cat ${OAUTH_CLIENT_SECRET_PATH})
 fi
 
-java -Djava.security.egd=file:/dev/./urandom -DoauthClientSecret=$oauthClientSecret -DkeystorePassword=$keystorePassword -jar app.jar $@
+if [ $dbPassword ]; then
+    MYSQL_PASSWORD_VAL=$dbPassword
+elif [ $dbPassword_file ]; then
+    MYSQL_PASSWORD_VAL=`cat $dbPassword_file`
+fi
+
+java -Djava.security.egd=file:/dev/./urandom -DoauthClientSecret=$oauthClientSecret -DkeystorePassword=$keystorePassword -DdbPassword=$MYSQL_PASSWORD_VAL -jar app.jar $@
 
 exec env "$@"
